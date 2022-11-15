df431e1b37a6d1e6281df138d06ee1068c05de33
ilDug <dognini.marco@gmail.com>
2022-11-15 10:42:50 +0100

    fixing meta definition

--- test/meta-example.php
+++ test/meta-example.php
@@ -1,21 +1,37 @@
-<head>
-    <!-- META -->
-    <?php
-    /** REQUIRED or set in  AppConfig class*/
-    \ilDug\Meta::$TEMPLATE = __DIR__ . "/path/to/tags.html";
+  <?php
+    class AppConfig
+    {
 
-    /** OPTIONAL or set in  AppConfig class*/
-    \ilDug\Meta::$PLACEHOLDERS = array('%TITLE%', '%DESCRIPTION%', '%MAIN_IMAGE%', '%URL%');
+        public function init()
+        {
+            \ilDug\Web\Meta::$TEMPLATE = __DIR__ . "/path/to/tags.html";
 
+            /** default */
+            \ilDug\Web\Meta::$PLACEHOLDERS = array('%TITLE%', '%DESCRIPTION%', '%MAIN_IMAGE%', '%URL%');
+        }
+    }
 
-    \ilDug\Meta::publish([
-        "Title",
-        "Description",
-        "https://website.it/path/to/images/main_image.jpg",
-        "https://website.it",
-    ]);
     ?>
 
-    <!-- STYLES  -->
-    <link rel="stylesheet" href="dist/styles.css">
-</head>
+
+  <head>
+      <!-- META -->
+      <?php
+        /** REQUIRED or set in  AppConfig class*/
+        \ilDug\Web\Meta::$TEMPLATE = __DIR__ . "/path/to/tags.html";
+
+        /** OPTIONAL or set in  AppConfig class*/
+        \ilDug\Web\Meta::$PLACEHOLDERS = array('%TITLE%', '%DESCRIPTION%', '%MAIN_IMAGE%', '%URL%');
+
+
+        \ilDug\Web\Meta::publish([
+            "Title",
+            "Description",
+            "https://website.it/path/to/images/main_image.jpg",
+            "https://website.it",
+        ]);
+        ?>
+
+      <!-- STYLES  -->
+      <link rel="stylesheet" href="dist/styles.css">
+  </head>
